ef3db8fa283c51c14a0a76f5ce808d99f6b61b01
jayanthavula <46263507+jayanthavula@users.noreply.github.com>
2025-01-05 12:45:11 +0530

    Update count_words.sh

--- count_words.sh
+++ count_words.sh
@@ -2,3 +2,4 @@
 #This script that reads a text file and counts the occurrences of each word, displaying the top 5 most frequent words along with their counts.
 file=/home/admin/scripts/words.txt
 sort "$file" | uniq -c | sort -nr | head -n 5
+sort "$file" | uniq -c | sort -nr | head -n 5 | awk '{print $2 "=" $1}' # to get output like word=count
